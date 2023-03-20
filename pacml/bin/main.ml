a812aeaf93703f6bd94b273ca489b160d0beee84
Arnav Agrawal <aa779@cornell.edu>
2023-03-20 18:34:27 -0400

    Edit main.ml
    - Added print_board method and a basic terminal functionality
    - implemented helper functions for getting a board from a csv file
    
    aa779, tks39, tp275 (Group coding)

--- pacml/bin/main.ml
+++ pacml/bin/main.ml
@@ -1,33 +1,48 @@
-(*open Game
+open Game
 open Command
-open Logic
-open State*)
+(*open Logic*)
+open State
 
 let data_dir_prefix = "data" ^ Filename.dir_sep
 
-(*type t =
+type t =
   | A of Csv.t
   | B of string
 
-  let get_file f =
-    try A (Csv.load f)
-    with _ -> B ("There is no file with path: " ^ f)
+let get_file f =
+  try A (Csv.load f)
+  with _ -> B ("There is no file with path: " ^ f)
   
-  let rec join_list = function
-    | [] -> ""
-    | [ s ] -> s
-    | s :: tl -> s ^ " " ^ join_list tl
-*)
-let play_game filename = print_string ("Looking for file " ^ filename)
+(*let rec join_list = function
+  | [] -> ""
+  | [ s ] -> s
+  | s :: tl -> s ^ " " ^ join_list tl
+ *)
+let rec print_line = function
+| [] -> print_string ""
+| hd :: tl -> print_string hd; print_line tl
 
-  let main () =
-      ANSITerminal.print_string [ ANSITerminal.red ]
-        "\n\nWelcome to the 3110 Text Adventure Game engine.\n";
-      print_endline "Please enter the name of the game file you want to load.\n";
-      print_string "> ";
-      match read_line () with
-      | exception End_of_file -> ()
-      | file_name -> play_game (data_dir_prefix ^ file_name ^ ".json")
-    
-    (* Execute the game engine. *)
-    let () = main ()
+let rec print_board (b: string list list) = match b with
+| [] -> print_string "\n"
+| hd :: tl -> print_line (hd @ ["\n"]); print_board tl
+let play_game g :unit = 
+  print_board (board g);
+  match read_line () |> parse with
+  _ -> print_string "Printed board correctly"
+
+let start_game filename : unit = let file = get_file filename in 
+  match file with
+  | A g -> play_game (init_state g)
+  | B m -> print_string m
+
+(** [main ()] prompts for the game to play, then starts it. *)
+let main () =
+    ANSITerminal.print_string [ ANSITerminal.red ] ("\n\nWelcome to Pacman!\n");
+    ANSITerminal.print_string [ ANSITerminal.blue ] "Please choose the difficulty (easy, medium, hard): \n";
+    print_string "> ";
+    match read_line () with
+    | exception End_of_file -> ()
+    | file_name -> start_game (data_dir_prefix ^ file_name ^ ".csv")
+
+(* Execute the game engine. *)
+let () = main ()
