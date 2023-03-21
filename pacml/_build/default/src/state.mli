62122a4eb233a126da7efc2003eebb6d10caa0ac
tsangha03 <tsangha03@gmail.com>
2023-03-20 23:21:19 -0400

    Added the medium and hard boards (tks39).

--- pacml/_build/default/src/state.mli
+++
@@ -1,7 +0,0 @@
-type t
-val init_state : Csv.t -> t
-val current : t -> int*int
-val lives: t -> int
-val cherries: t -> (int*int) list
-val ghosts: t -> (int*int) list
-val board: t -> string list list
--- pacml/src/command.mli
+++ pacml/src/command.mli
@@ -1,14 +1,19 @@
 (** Parsing of player commands. *)
 
-type dir = Up | Left | Down | Right
-(** the type [dir] represents the direction in which pacman will move*)
+type dir =
+  | Up
+  | Left
+  | Down
+  | Right
+      (** the type [dir] represents the direction in which pacman will move*)
 
-type command = 
-| Move of dir
-| Pause
-| Quit
-(** The type [command] represents a player command that is decomposed into a
-    verb and possibly a direction. Invariant: [Move] must have a non-empty direction*)
+type command =
+  | Move of dir
+  | Pause
+  | Quit
+      (** The type [command] represents a player command that is decomposed into
+          a verb and possibly a direction. Invariant: [Move] must have a
+          non-empty direction*)
 
 exception Empty
 (** raised when an empty command is parsed*)
