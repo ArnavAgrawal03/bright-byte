f4388d8393dbf5ce654ab028e9060900927ff43b
Arnav Agrawal <aa779@cornell.edu>
2023-05-11 16:56:36 -0400

    Add ghost initialization

--- pacml/src/Ghost.ml
+++ pacml/src/Ghost.ml
@@ -6,10 +6,25 @@ type color =
 
 type t = {
   pos : Board.position;
-  og_pos : Baord.position;
-  dir : Command.move;
+  og_pos : Board.position;
   color : color;
+  dir : Command.dir;
   active : bool;
   scatter : bool;
   scatter_frames : int;
 }
+
+let init pos color dir =
+  {
+    pos;
+    og_pos = pos;
+    color;
+    dir;
+    active = true;
+    scatter = false;
+    scatter_frames = 0;
+  }
+
+let pos_of t = t.pos
+let color_of t = t.color
+let move t pos = { t with pos }
--- pacml/src/Ghost.mli
+++ pacml/src/Ghost.mli
@@ -5,3 +5,17 @@ type color =
   | Yellow
 
 type t
+
+val init : Board.position -> color -> Command.dir -> t
+(** [init p c d] is a ghost with initial position [p], color [c], and pointing
+    in direction [d]*)
+
+val pos_of : t -> Board.position
+(** [pos_of g] is the current position of ghost [g]*)
+
+val color_of : t -> color
+(** [color_of g] is the color of ghost [g]*)
+
+val move : t -> Board.position -> t
+(** [move g p] is a ghost [g'] with same attributes as [g] but with current
+    position [p]*)
